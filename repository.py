f61a776bac9daa1afa60226bc09bb4778fc22b60
HuKuTa-y <zelleonid73@gmail.com>
2025-09-17 14:28:29 +0300

    Update repository.py

--- repository.py
+++ repository.py
@@ -1,80 +1,80 @@
-from flight import Flight
+from ticket import Ticket
 from database import DatabaseConnection
 
 
-class FlightRepository:
+class TicketRepository:
     '''Класс-репозиторий для доступа к БД'''
 
-    def __init__(self,connection: DatabaseConnection):
-        self.connection=connection
+    def __init__(self, connection: DatabaseConnection):
+        self.connection = connection
 
-    def create_flight(self, flight:Flight):
-        """Добавление рейса"""
+    def create_ticket(self, ticket: Ticket):
+        """Добавление билета"""
 
         conn = self.connection.get_connection()
         cursor = conn.cursor()
 
         cursor.execute('''
-            INSERT INTO flights
-                        (plane,price)
-                        VALUES (%s,%s)
-            ''',(flight.plane,flight.price))
+            INSERT INTO tickets
+                        (plane,price, name_movie, price)
+                        VALUES (%s,%s,%s,%s)
+            ''', (ticket.row, ticket.place, ticket.name_movie, ticket.price ))
         conn.commit()
 
         cursor.close()
         conn.close()
 
-        return flight
-    
+        return ticket
+
     def get_all(self):
         conn = self.connection.get_connection()
         cursor = conn.cursor()
-        cursor.execute("SELECT * FROM flights ORDER BY id")
+        cursor.execute("SELECT * FROM tickets ORDER BY id")
         rows = cursor.fetchall()
 
-        flights = []
+        tickets = []
         for row in rows:
-            flights.append(Flight(
+            tickets.append(Ticket(
                 row[0],
                 row[1],
                 row[2]
             ))
-              
+
         cursor.close()
         conn.close()
-        return flights
-        
-    def get_by_id(self,flight_id:int):
-        """Получить рейс по идентификатору"""
+        return tickets
+
+    def get_by_id(self, ticket_id: int):
+        """Получить билет по идентификатору"""
         conn = self.connection.get_connection()
         cursor = conn.cursor()
 
-        cursor.execute("SELECT * FROM flights WHERE id = %s",(flight_id,))
+        cursor.execute("SELECT * FROM tickets WHERE id = %s", (ticket_id,))
         row = cursor.fetchone()
-        
+
         cursor.close()
         conn.close()
 
         if row:
-            return Flight(
+            return Ticket(
                 row[0],
                 row[1],
                 row[2]
             )
         return None
-    
-    def update_flight(self, flight:Flight):
-        """Изменить существующий рейс. 
+
+    def update_ticket(self, ticket: Ticket):
+        """Изменить существующий рейс.
             Если рейса не существует, ничего не делать."""
         conn = self.connection.get_connection()
         cursor = conn.cursor()
-        
+
         cursor.execute('''
             UPDATE flights
             SET price = %s, plane = %s
             WHERE id = %s
-            ''',(flight.price, flight.plane, flight.id))
-        
+            ''', (flight.price, flight.plane, flight.id))
+
         result = cursor.fetchone()
         flight.id = result[0]
         conn.commit()
@@ -83,19 +83,19 @@ class FlightRepository:
         conn.close()
 
         return flight
-    
-    def delete_flight(self,flight_id:int):
+
+    def delete_flight(self, flight_id: int):
         """Удалить существующий рейс.
             Если рейса не существует, ничего не делать."""
         conn = self.connection.get_connection()
         cursor = conn.cursor()
         cursor.execute('''
             DELETE FROM flights WHERE id = %s
-            ''',(flight_id,))
+            ''', (flight_id,))
         conn.commit()
         deleted = cursor.rowcount
 
         cursor.close()
         conn.close()
 
-        return deleted >0
+        return deleted > 0
